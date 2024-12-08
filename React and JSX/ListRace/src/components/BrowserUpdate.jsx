c8dc4cff39d83cdb39e33947525dab1bdd9eaf51
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-12-08 03:09:18 +0200

    Add component

---
+++ React and JSX/ListRace/src/components/BrowserUpdate.jsx
@@ -0,0 +1,6 @@
+export default function BrowserUpdate() {
+    return(
+        <p className="browserupgrade">You are using an <strong>outdated</strong>
+            browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    );
+}
