b37ce702eed2f80e089aa071357c44b7ed28d0c4
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-12-10 00:06:42 +0200

    Add To-Do app

---
+++ todo-list/client/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Foot from "./components/Foot"
+import Main from "./components/Main"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Foot />
+    </>
+  )
+}
+
+export default App
---
+++ todo-list/client/src/components/Foot.jsx
@@ -0,0 +1,7 @@
+export default function Foot() {
+    return (
+        <footer className="footer">
+            <p>Copyright © designed by Mihail Valkov</p>
+        </footer>
+    );
+}
