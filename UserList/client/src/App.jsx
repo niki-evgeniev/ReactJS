5a87f4ee097784ecf294dcd2d83371b9d3999705
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-12-18 01:18:27 +0200

    add components

--- UserList/client/src/App.jsx
+++ UserList/client/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from './components/footer/footer';
+import Header from './components/header/Header';
+import UserSection from './components/user-section/UserSection';
+
+import './styles.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+
+      <main className='main'>
+        <UserSection />
+      </main>
+      
+      <Footer />
     </>
   )
 }
